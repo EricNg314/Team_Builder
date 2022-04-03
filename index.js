43e11d63c8400ac39e0afd37552b05989ac0d9fe
Okens <Ng.Eric314@gmail.com>
2022-04-02 19:40:13 -0700

    Updating with menu and loop.

--- index.js
+++ index.js
@@ -8,15 +8,71 @@ const Intern = require("./lib/Intern");
 async function buildHTML() {
   let htmlInfoStr = "";
   const filename = "./sample/index.html";
-  const managerStr = await buildManager();
+  // const managerStr = await buildManager();
+  // const engineerStr = await buildEngineer();
 
-  htmlInfoStr += `${managerStr}`;
+  const employeesStr = await selectEmployeeMenu();
+
+  htmlInfoStr += `${employeesStr}`;
   console.log(htmlInfoStr);
   fs.writeFile(filename, htmlInfoStr, (err) =>
     err ? console.log(err) : console.log(`Success! Please check ${filename}`)
   );
 }
 
+let needEmployee = true;
+const selectEmployeeMenu = async () => {
+  let infoStr = "";
+  // let needEmployee = true;
+  while (needEmployee === true) {
+    await inquirer
+      .prompt([
+        {
+          name: "role",
+          type: "list",
+          message: "What role type?",
+          choices: ['Manager','Engineer','Intern'],
+          validate: (answer) => {
+            if (answer) {
+              return true;
+            } else {
+              console.log("Please select a type.");
+            }
+          }
+        }
+      ])
+      .then(async (data) => {
+        const { role, moreEmployees } = data;
+        if(role == 'Manager'){
+          infoStr = await buildManager();
+        } else if(role == 'Engineer'){
+          infoStr = await buildEngineer();
+        } else if(role == 'Intern'){
+          infoStr = await buildIntern();
+        }
+        needEmployee = await addMore();
+        console.log('needEmployee? ', needEmployee)
+      });
+    }
+  return infoStr;
+}
+
+const addMore = async () => {
+  let addMoreEmp = false;
+  await inquirer
+    .prompt([
+      {
+        name: "moreEmployees",
+        type: "confirm",
+        message: "Add more employees?",
+      }
+    ])
+    .then(async (data) => {
+      const { moreEmployees } = data;
+      addMoreEmp = moreEmployees
+    });
+    return addMoreEmp;
+}
 
 const buildManager = async () => {
   let managerStr = "";
@@ -25,14 +81,14 @@ const buildManager = async () => {
       {
         name: "name",
         type: "input",
-        message: "Name:",
+        message: "Manager Name:",
         validate: (answer) => {
           if (answer) {
             return true;
           } else {
             console.log("Please provide a name.");
           }
-        },
+        }
       },
       {
         name: "empId",
@@ -44,7 +100,7 @@ const buildManager = async () => {
           } else {
             console.log("Please provide a employee ID.");
           }
-        },
+        }
       },
       {
         name: "email",
@@ -56,7 +112,7 @@ const buildManager = async () => {
           } else {
             console.log("Please provide a email.");
           }
-        },
+        }
       },
       {
         name: "officeNumb",
@@ -68,7 +124,7 @@ const buildManager = async () => {
           } else {
             console.log("Please provide an office number.");
           }
-        },
+        }
       },
     ])
     .then((data) => {
@@ -82,6 +138,73 @@ const buildManager = async () => {
       <p>${manager.officeNumber}</p>`;
     });
   return managerStr;
+};
+
+const buildEngineer = async () => {
+  let engineerStr = "";
+  await inquirer
+    .prompt([
+      {
+        name: "name",
+        type: "input",
+        message: "Engineer Name:",
+        validate: (answer) => {
+          if (answer) {
+            return true;
+          } else {
+            console.log("Please provide a name.");
+          }
+        }
+      },
+      {
+        name: "empId",
+        type: "input",
+        message: "Employee ID:",
+        validate: (answer) => {
+          if (answer) {
+            return true;
+          } else {
+            console.log("Please provide a employee ID.");
+          }
+        }
+      },
+      {
+        name: "email",
+        type: "input",
+        message: "Email:",
+        validate: (answer) => {
+          if (answer) {
+            return true;
+          } else {
+            console.log("Please provide a email.");
+          }
+        }
+      },
+      {
+        name: "gitHub",
+        type: "input",
+        message: "Github username:",
+        validate: (answer) => {
+          if (answer) {
+            return true;
+          } else {
+            console.log("Please provide an github username.");
+          }
+        }
+      }
+    ])
+    .then((data) => {
+      const { name, empId, email, gitHub } = data;
+      const engineer = new Engineer(name, empId, email);
+      engineer.addGithub(gitHub);
+      engineerStr = `
+      <p>${engineer.name}</p>
+      <p>${engineer.employeeID}</p>
+      <p>${engineer.emailAddress}</p>
+      <p>${engineer.ghUserName}</p>
+      <p>${engineer.ghLink}</p>`;
+    });
+  return engineerStr;
 };
 
 
